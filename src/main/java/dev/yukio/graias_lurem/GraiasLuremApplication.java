590cbe375f67fe5dbe68333035d1d0c597838eec
Vinicius_Vatanabi <viniciusvatanabi@gmail.com>
2023-02-04 20:01:48 -0300

    Initial commit

---
+++ src/main/java/dev/yukio/graias_lurem/GraiasLuremApplication.java
@@ -0,0 +1,13 @@
+package dev.yukio.graias_lurem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraiasLuremApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraiasLuremApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/yukio/graias_lurem/GraiasLuremApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.yukio.graias_lurem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraiasLuremApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
